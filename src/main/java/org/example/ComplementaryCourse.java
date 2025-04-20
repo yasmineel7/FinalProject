b165b333d83da87f59b58fb3c7cc9b5c6ea7c5a3
yasmi <6298686@edu.vaniercollege.qc.ca>
2025-04-20 09:59:39 -0400

    project

--- src/main/java/org/example/ComplementaryCourse.java
+++ src/main/java/org/example/ComplementaryCourse.java
@@ -2,4 +2,10 @@ package org.example;
 
 public class ComplementaryCourse extends Course{
     protected String code;
+
+    @Override
+    public int compareTo(Course o) {
+        //TODO: sort the courses depending on the code
+        return super.compareTo(o);
+    }
 }
--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -2,12 +2,18 @@ package org.example;
 
 import java.util.Set;
 
-public class Course {
+public class Course implements Comparable<Course>{
 
     protected Set<Course> courses;
 
     public static double calculateFee() {
         //TODO: calculate fee for all the courses chosen (mandatory or complementary)
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Course o) {
+        //TODO: sort the courses (mandatory and complementary) depending on each of their code
         return 0;
     }
 }

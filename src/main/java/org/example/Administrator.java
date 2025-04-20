6c2a8c35deeedd873296441b9d8fd4680e33add3
yasmi <6298686@edu.vaniercollege.qc.ca>
2025-04-19 22:09:09 -0400

    project

---
+++ src/main/java/org/example/Administrator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.LinkedList;
+
+public class Administrator extends Users{
+    public Administrator(String name, int id) {
+        super(name, id);
+    }
+
+    private static LinkedList<Student> readTranscript(String path) {
+        //TODO: read the transcript odf the students
+    }
+}
---
+++ src/main/java/org/example/Users.java
@@ -0,0 +1,15 @@
+package org.example;
+
+abstract class Users {
+    protected String name;
+    protected int id;
+
+    public Users(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    void PrintDetail() {
+
+    }
+}

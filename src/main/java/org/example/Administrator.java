52f174c17284d46fd870b2009d4aadb6e8e3fe92
yasmi <6298686@edu.vaniercollege.qc.ca>
2025-04-20 09:10:38 -0400

    project

--- src/main/java/org/example/Administrator.java
+++ src/main/java/org/example/Administrator.java
@@ -1,3 +1,17 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
+public class Administrator {
+
+
+    public Administrator(String name, int id) {
+        super();
+    }
+
+    private static LinkedList<String> readTranscript(String path) {
+        //TODO: read the transcript odf the students
+        return null;
+    }
 }

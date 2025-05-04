9a9179f07c05c84a01d7de409fd23cf63bc97722
yasmi <6298686@edu.vaniercollege.qc.ca>
2025-05-04 15:21:21 -0400

    adding the toString, equals and hashcode

--- src/main/java/org/example/ComplementaryCourse.java
+++ src/main/java/org/example/ComplementaryCourse.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class ComplementaryCourse extends Course{
@@ -15,7 +16,6 @@ public class ComplementaryCourse extends Course{
     public void calculateFee(Set<Course> courses) {
        super.calculateFee(courses);
     }
-
 
     @Override
     public void enroll(Student s) {
@@ -33,4 +33,31 @@ public class ComplementaryCourse extends Course{
         return super.compareTo(o);
     }
 
+    @Override
+    public String toString() {
+        return "ComplementaryCourse{" +
+                "choice='" + choice + '\'' +
+                '}' + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ComplementaryCourse that = (ComplementaryCourse) o;
+        return Objects.equals(choice, that.choice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), choice);
+    }
+
+    public String getChoice() {
+        return choice;
+    }
+
+    public void setChoice(String choice) {
+        this.choice = choice;
+    }
 }

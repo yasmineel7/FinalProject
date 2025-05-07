fe87d58e89cd9ae1f29a45cac50737af7eae6b51
yasmi <6298686@edu.vaniercollege.qc.ca>
2025-05-07 11:25:23 -0400

    adding a variable choice and removing a set od MandatoryCourse. Also, adding the hashcode, equals, toString, getter and setter

--- src/main/java/org/example/MandatoryCourse.java
+++ src/main/java/org/example/MandatoryCourse.java
@@ -1,14 +1,15 @@
 package org.example;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class MandatoryCourse extends Course{
-    private Set<Course> MandatoryCourses;
+    String choice;
 
-
-    public MandatoryCourse(String code, String nameCourse, int priceCourse, List<Student> students) {
+    public MandatoryCourse(String code, String nameCourse, int priceCourse, List<Student> students, String choice) {
         super(code, nameCourse, priceCourse, students);
+        this.choice = choice;
     }
 
     @Override
@@ -31,4 +32,31 @@ public class MandatoryCourse extends Course{
         return super.compareTo(o);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        MandatoryCourse that = (MandatoryCourse) o;
+        return Objects.equals(choice, that.choice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), choice);
+    }
+
+    @Override
+    public String toString() {
+        return "MandatoryCourse{" +
+                "choice='" + choice + '\'' +
+                '}' + super.toString();
+    }
+
+    public String getChoice() {
+        return choice;
+    }
+
+    public void setChoice(String choice) {
+        this.choice = choice;
+    }
 }
